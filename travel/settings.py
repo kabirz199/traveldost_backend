516a5f347485056ccbeb35eef16838a0d020fb2b
Amalnath <amalnaths640@gmail.com>
2025-05-25 16:27:59 +0530

    Initial commit

--- travel/settings.py
+++ travel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jx+!5y&@hii1g#ti0rmyib@$&s99u)b$kmwhm4eqyra_n!!fjq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
